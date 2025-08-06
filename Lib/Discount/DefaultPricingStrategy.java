859e9750685789662a18c79b11849e5422b7506f
Kittipat13 <kittipat.li@ku.th>
2025-08-06 18:46:49 +0700

    Add files via upload

---
+++ Lib/Discount/DefaultPricingStrategy.java
@@ -0,0 +1,9 @@
+package Lib.Discount;
+import Lib.CartItem;
+
+public class DefaultPricingStrategy implements DiscountStrategy {
+    @Override
+    public double calculatePrice(CartItem item) {
+        return item.getProduct().getPrice() * item.getQuantity();
+    }
+}
